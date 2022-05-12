ea751b831557e695a05b073956b24e050b7e9496
Ronan <r.dhersignerie@gmail.com>
2022-05-12 16:34:37 +0200

    add boss animation & logics

--- Assets/Boss/Scripts/BossController.cs
+++ Assets/Boss/Scripts/BossController.cs
@@ -1,47 +1,109 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
-
-public class BossController : MonoBehaviour
+using UnityExtendedEditor.Attributes;
+using System.Collections;
+static class BossAnimationNames
 {
+    public static string Spell {get;} = "Spell";
+    public static string Death {get;} = "Sword And Shield Death";
+    public static string Idle {get;} = "Sword And Shield Idle";
+    public static string Impact {get;} = "Sword And Shield Impact";
+    public static string Kick {get;} = "Kick";
+    public static string Power {get;} = "Sword And Shield Power Up";
+    public static string Run {get;} = "Running";
+    public static string Slash {get;} = "Sword And Shield Slash";
 
-    [Header("Kick")] 
-    [SerializeField] 
-    private float distanceKick = 5f;
-    private float distanceKickProjection = 5f;
-    private float kickDamages = 2.5f;
-    
-    [Header("Sword")] 
-    [SerializeField] 
-    private float distanceSword = 10f;
-    private float sowrdDamages = 5f;
+}
 
-    [Header("Spell")] 
-    [SerializeField] 
-    private float distanceSpell = 20f;
-    private float spellDamages = 5f;
+public class BossController : LineOfSight
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Animator animator;
+    [Header("Movement")] [SerializeField] private float speed = 10f;
+
+    [SerializeField] private float selfSpace = 3f;
+
+    [Header("Kick")] [SerializeField] [MinMaxSlider(0.1f, 20)]
+    private Vector2 kickRange= new Vector2(0f,5f);
+    [SerializeField] private float distanceKickProjection = 5f;
+    [SerializeField] private float kickDamages = 2.5f;
+    [SerializeField] private Color kickColor = Color.blue;
     
+    public Vector2 KickRange => kickRange;
+    public float DistanceKickProjection => distanceKickProjection;
+    public float KickDamages => kickDamages;
+    public Color KickColor => kickColor;
+
+    [Header("Sword")] [SerializeField] [MinMaxSlider(1, 20)]
+    private Vector2 swordRange = new Vector2(0f,5f);
+    [SerializeField] private float sowrdDamages = 5f;
+    [SerializeField] private Color swordColor = Color.cyan;
     
-    
-    void AttackCastSpell()
+    public Vector2 SwordRange => swordRange;
+    public float SowrdDamages => sowrdDamages;
+    public Color SwordColor => swordColor;
+
+    [Header("Spell")] [SerializeField] [MinMaxSlider(1, 20)]
+    private Vector2 spellRange = new Vector2(10f,20f);
+    [SerializeField] private float spellDamages = 5f;
+    [SerializeField] private Color spellColor = Color.magenta;
+    [SerializeField] [Range(2f, 10f)] private float spellCooldown = 7f;
+    private bool canCastSpell = true;
+    public Vector2 SpellRange => spellRange;
+    public float SpellDamages => spellDamages;
+
+    public float SpellCooldown => spellCooldown;
+    public Color SpellColor => spellColor;
+
+
+    private void Update()
     {
-        
+        AttackKick();
     }
 
-    void AttackKick()
+    void ReachPlayer()
     {
-        
+        Vector3 targetPos = new Vector3(target.position.x,
+            transform.position.y,
+            target.position.z);
+        transform.LookAt(targetPos);
+        if (Vector3.Distance(transform.position, targetPos) > selfSpace)
+        {
+            transform.position += transform.forward * (Time.deltaTime * speed);
+             animator.SetBool(BossAnimationNames.Run, true);
+        }
+        else
+        {
+            animator.SetBool(BossAnimationNames.Run, false);
+        }
+    }
+
+    void AttackCastSpell()
+    {
+        float distanceTarget = Vector3.Distance(target.position, transform.position);
+        if (canCastSpell && (spellRange.x <= distanceTarget && distanceTarget <= spellRange.y))
+        {
+            canCastSpell = false;
+            animator.SetTrigger(BossAnimationNames.Spell);
+            StartCoroutine(ResetSpellStatus());
+        } 
+    }
+    IEnumerator ResetSpellStatus()
+    {
+        yield return new WaitForSeconds(spellCooldown);
+        canCastSpell = true;
+    }
+    
+    void AttackKick()
+    {        
+        float distanceTarget = Vector3.Distance(target.position, transform.position);
+        if ((kickRange.x <= distanceTarget && distanceTarget <= kickRange.y))
+        {
+            animator.SetTrigger(BossAnimationNames.Kick);
+        } 
     }
 
     void AttackSword()
     {
-        
     }
-    
-    void RunToPlayer()
-    {
-        
-    }
-    
+
 }
