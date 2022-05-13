0cd874d102148c83cd0db4625d1445e6c3d201e1
Aedrinios <clementclerc2@gmail.com>
2022-05-13 17:04:00 +0200

    push scene

--- Assets/Boss/Scripts/BossTree.cs
+++ Assets/Boss/Scripts/BossTree.cs
@@ -9,11 +9,12 @@ public class BossTree : Tree
     [SerializeField] private BossController boss;
     protected override Node SetupTree()
     {
-        CheckBetween checkDist = new CheckBetween(boss.transform, boss.Target, boss.SwordRange);
+        CheckBetween checkSwordDist = new CheckBetween(boss.transform, boss.Target, boss.SwordRange);
+        CheckBetween checkKickDist = new CheckBetween(boss.transform, boss.Target, boss.KickRange);
         AttackSword attackSword = new AttackSword(boss);
         ReachPlayer reachPlayer = new ReachPlayer(boss, boss.Target);
 
-        Sequence attackSequence = new Sequence(new List<Node>() { checkDist, attackSword }, identifier);
+        Sequence attackSequence = new Sequence(new List<Node>() { checkSwordDist, attackSword }, identifier);
         Selector bossSelector = new Selector(new List<Node>() { attackSequence, reachPlayer }, identifier);
 
         return bossSelector;
