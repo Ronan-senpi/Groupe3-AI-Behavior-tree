8af104e599ba26217153a1f5b7170e27de436608
Ronan <r.dhersignerie@gmail.com>
2022-05-13 15:16:30 +0200

    PLayer attaque

--- Assets/Boss/Scripts/Hitboxes/HitboxController.cs
+++ Assets/Boss/Scripts/Hitboxes/HitboxController.cs
@@ -2,38 +2,50 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 [RequireComponent(typeof(Collider))]
 public class HitboxController : MonoBehaviour
 {
-    public float Duration { get; set; } = 1f;
+    public float? Duration { get; set; }
     public float Damage { get; set; }
     public LayerMask TargetMask { get; set; }
+    public Collider Col { get; set; }
+    private void Awake()
+    {
+        Col = GetComponent<Collider>();
+    }
+
     private void OnEnable()
     {
-        StartCoroutine(Desactivate());
+        if(Duration.HasValue)
+            StartCoroutine(Desactivate());
     }
 
     IEnumerator Desactivate()
     {
-        yield return new WaitForSeconds(Duration);
+        yield return new WaitForSeconds(Duration.Value);
         gameObject.SetActive(false);
     }
 
-    protected virtual void customAction(FightScript fs)
+    protected virtual void customAction(HealthController fs)
     {
-        
-    } 
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         //Compare sur les bit et non sur la text plus rapide
         if ((TargetMask.value & (1 << other.gameObject.layer)) > 0)
         {
-          FightScript  fs = other.gameObject.GetComponent<FightScript>();
-          if (fs != null && fs.Rb != null)
-          {
-              customAction(fs);
-              fs.TakeDamage(Damage);
-          }
+            HealthController hc = other.gameObject.GetComponent<HealthController>();
+            if (hc != null)
+            {
+                customAction(hc);
+                hc.Impact(Damage);
+            }
+            else
+            {
+                Debug.LogError("HealthController non trouvé");
+            }
         }
     }
 }
